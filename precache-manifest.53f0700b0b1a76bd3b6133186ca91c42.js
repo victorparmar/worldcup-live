d51e005c251ebe8ff1aa509c815afd3bc7389108
Victor Parmar <vic@smalldata.tech>
2019-02-01 20:14:18 +0100

    Updates

---
+++ precache-manifest.53f0700b0b1a76bd3b6133186ca91c42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d86198c1c487d5a4869a",
+    "url": "/worldcup-live/static/js/runtime~main.d86198c1.js"
+  },
+  {
+    "revision": "40030b076e79340ca51d",
+    "url": "/worldcup-live/static/js/main.40030b07.chunk.js"
+  },
+  {
+    "revision": "17a7380fd07424d4879c",
+    "url": "/worldcup-live/static/js/1.17a7380f.chunk.js"
+  },
+  {
+    "revision": "40030b076e79340ca51d",
+    "url": "/worldcup-live/static/css/main.f172826e.chunk.css"
+  },
+  {
+    "revision": "b7af28fc34f37d1afb63b86d6a9e42d7",
+    "url": "/worldcup-live/index.html"
+  }
+];
