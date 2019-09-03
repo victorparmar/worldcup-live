200c399a2e6099e12f5a38a8225a9e53492ed552
Victor Parmar <vic@smalldata.tech>
2019-09-03 11:24:52 +0200

    Updates

---
+++ precache-manifest.a45ab83be8cdd87798a1115254261cd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af340e5630b0709a2aa2819de4d3a12",
+    "url": "/worldcup-live/index.html"
+  },
+  {
+    "revision": "2f64c66b9d1b0a5c53a3",
+    "url": "/worldcup-live/static/css/main.475721f9.chunk.css"
+  },
+  {
+    "revision": "859346120ab0ffba1d6d",
+    "url": "/worldcup-live/static/js/2.f9e70659.chunk.js"
+  },
+  {
+    "revision": "2f64c66b9d1b0a5c53a3",
+    "url": "/worldcup-live/static/js/main.120c8ba6.chunk.js"
+  },
+  {
+    "revision": "6429faa6fcdef0721be2",
+    "url": "/worldcup-live/static/js/runtime~main.e9e5906b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/worldcup-live/precache-manifest.33be4a69233cefde6bcfdd9fefc1fa39.js"
+  "/worldcup-live/precache-manifest.a45ab83be8cdd87798a1115254261cd4.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/worldcup-live/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/worldcup-live/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
